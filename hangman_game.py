f6e891a0feadd0eef575e2f971093fab7234cda7
Vadlamudi Puneeth <118372146+Vadlamudi-Puneeth@users.noreply.github.com>
2023-12-25 10:38:53 +0530

    Update hangman_game.py

--- hangman_game.py
+++ hangman_game.py
@@ -14,20 +14,20 @@ game_over = False
 
 while(not game_over):
     user_input = input("Guess the letter: \n")
-    
+
     for position in range(len(random_item)):
         letter = random_item[position]
         if (letter == user_input):
             empty_list[position] = user_input
     print(empty_list)
-    
+
     if (user_input not in random_item):
         lives = lives - 1
-        
+        print("You have left with ",lives," lives")
         if(lives == 0):
             game_over = True
-            print("\nyou lose\n")
-            
+            print("\nlives are zero \nyou lose\n")
+
     if "_" not in empty_list:
         game_over = True
-        print("\nyou win\n")
+        print("\nCongratulations you win the game\n")
